0bbb8483e391b91c06ccf276f0d247d32c600451
Humorloos <l.loos95@gmail.com>
2019-09-01 19:02:54 +0200

    add %s to X_CARD_BTN and X_CARD_SR1 to allow usage both in card front and back

--- consts.py
+++ consts.py
@@ -1,8 +1,8 @@
-
 X_MODEL_NAME = 'Stepwise Map Retrieval'
 
 X_MAX_ANSWERS = 20
-X_CARD_NAMES = list(map(lambda aswid: 'Answer ' + str(aswid), list(range(1, X_MAX_ANSWERS + 1))))
+X_CARD_NAMES = list(map(lambda aswid: 'Answer ' + str(aswid),
+                        list(range(1, X_MAX_ANSWERS + 1))))
 
 X_FLDS = {
     'id': 'ID',
@@ -13,44 +13,45 @@ for i in range(1, X_MAX_ANSWERS + 1):
 X_FLDS.update({
     'rf': 'Reference',
     'mt': 'Meta'
-               })
+})
 
 X_FLDS_IDS = ['id', 'qt'] + \
-             list(map(lambda aswid: 'a' + str(aswid), list(range(1, X_MAX_ANSWERS + 1)))) + \
+             list(map(lambda aswid: 'a' + str(aswid),
+                      list(range(1, X_MAX_ANSWERS + 1)))) + \
              ['rf', 'mt']
 
 # Elements for creating Card-Fronts
 
 # Header (all Cards)
-X_CARD_FRONT_HD = """<div class="reference">
+X_CARD_HD = """<div class="reference">
     {{%(ref)s}}
 </div>
 <span id="s1" style='display:none'>
     {{%(meta)s}}
 </span>
 <script>""" % \
-                  {'ref': X_FLDS['rf'],
-                   'meta': X_FLDS['mt']}
+            {'ref': X_FLDS['rf'],
+             'meta': X_FLDS['mt']}
 
 # JavaScript Card 1
-X_CARD_FRONT_SR1 = """
+X_CARD_SR1 = """
     var meta = JSON.parse(document.getElementById("s1").textContent);
     var nAnswers = meta.answers.length;
     if(nAnswers > 1) {
-        document.getElementById("h").innerHTML = "(1 / " + nAnswers + ")";
-        document.getElementById("dots").innerHTML = '<li><span class="dots">...</span></li>';
+        document.getElementById("h").innerHTML = "(1 / " + nAnswers + ")"; 
+        document.getElementById("dots").innerHTML = '<li><span class="dots"> %s </span></li>';
     } else {
-        document.getElementById("dots").innerHTML = '<span class="dots">...</span>';
+        document.getElementById("dots").innerHTML = '<span class="dots"> %s </span>';
     }"""
 
 # JavaScript Card N
-X_CARD_FRONT_SRN = """
+X_CARD_SRN = """
     var meta = JSON.parse(document.getElementById("s1").textContent);
     var nAnswers = meta.answers.length;
     document.getElementById("h").innerHTML = "(%s / " + nAnswers + ")";"""
 
 # Question (All Cards)
-X_CARD_FRONT_QT = """
+X_CARD_QT = """
 </script>
 <hr id="question">
 {{%s}}
@@ -59,20 +60,20 @@ X_CARD_FRONT_QT = """
 """ % X_FLDS['qt']
 
 # Bottom (Card 1)
-X_CARD_FRONT_BT1 = """<hr id="answer">
+X_CARD_BT1 = """<hr id="answer">
 <span id="dots">
 </span>"""
 
 # Bottom (Card n)
-X_CARD_FRONT_BTN = """<hr id="answer">
+X_CARD_BTN = """<hr id="answer">
 <li>
     <span class="dots">
-        ...
+        %s
     </span>
 </li>"""
 
 # Answer hint Card n
-X_CARD_FRONT_HT = """<li>
+X_CARD_HT = """<li>
     <span class="reference">
         {{%s}}
     </span>
