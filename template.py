373fc0e6241e7c03226c85e46fa49acbec632b8e
Humorloos <l.loos95@gmail.com>
2019-09-01 19:01:36 +0200

    adjust get_card() to return both front and back template

--- template.py
+++ template.py
@@ -1,13 +1,24 @@
 from consts import *
 
-def get_card_front(cid):
+# receives an id that represents the card to be created and returns a list with
+# front and back template
+def get_card(cid):
+    asw = '{{' + X_FLDS[X_FLDS_IDS[cid + 1]] + '}}'
     if cid == 1:
-        card_front = X_CARD_FRONT_HD + X_CARD_FRONT_SR1 + X_CARD_FRONT_QT + X_CARD_FRONT_BT1
+        card_front = X_CARD_HD + X_CARD_SR1 % ('...', '...') + \
+                     X_CARD_QT + X_CARD_BT1
+        card_back = X_CARD_HD + X_CARD_SR1 % (asw, asw) + \
+                    X_CARD_QT + X_CARD_BT1
     else:
-        # hint = '    '
         hint = ''
         for x in range(1, cid):
-            hint = hint + X_CARD_FRONT_HT % X_FLDS[X_FLDS_IDS[x + 1]]
-        card_front = '{{#' + X_FLDS[X_FLDS_IDS[cid + 1]] + '}}\n' + X_CARD_FRONT_HD + X_CARD_FRONT_SRN % cid + \
-                     X_CARD_FRONT_QT + hint + X_CARD_FRONT_BTN + '\n{{/' + X_FLDS[X_FLDS_IDS[cid + 1]] + '}}'
-    return card_front
+            hint = hint + X_CARD_HT % X_FLDS[X_FLDS_IDS[x + 1]]
+        card_front = '{{#' + X_FLDS[X_FLDS_IDS[cid + 1]] + '}}\n' + \
+                     X_CARD_HD + X_CARD_SRN % cid + \
+                     X_CARD_QT + hint + X_CARD_BTN % '...' + \
+                     '\n{{/' + X_FLDS[X_FLDS_IDS[cid + 1]] + '}}'
+        card_back = '{{#' + X_FLDS[X_FLDS_IDS[cid + 1]] + '}}\n' + \
+                    X_CARD_HD + X_CARD_SRN % cid + \
+                    X_CARD_QT + hint + X_CARD_BTN % asw + \
+                    '\n{{/' + X_FLDS[X_FLDS_IDS[cid + 1]] + '}}'
+    return [card_front, card_back]
