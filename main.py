946dba345dfb10dd666f02c225283f236ee4b784
Humorloos <l.loos95@gmail.com>
2019-09-02 12:03:38 +0200

    add on_profile_loaded() as hook of profileLoaded

---
+++ main.py
@@ -0,0 +1,12 @@
+from anki.hooks import addHook
+
+from config import *
+
+
+# creates smr model when loading profile if necessary
+def on_profile_loaded():
+    get_or_create_model()
+
+
+# Add-on setup at profile-load time
+addHook("profileLoaded", on_profile_loaded())
