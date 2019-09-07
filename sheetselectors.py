5b24dfc6779b4fc57247dd3e3a12602e4df99355
Humorloos <l.loos95@gmail.com>
2019-09-07 19:41:43 +0200

    add class SingleSheetSelector

---
+++ sheetselectors.py
@@ -0,0 +1,59 @@
+# Qt Dialogs for selecting and naming sheets to import
+from PyQt5 import QtCore, QtWidgets
+
+from xsheet import SheetElement
+
+from aqt.qt import *
+import aqt
+
+from XmindImport.consts import ICONS_PATH
+
+
+
+class SingleSheetSelector(QDialog):
+    def __init__(self, sheet: SheetElement):
+        parent = aqt.mw.app.activeWindow()
+        super().__init__(parent=parent)
+        self.sheet = sheet
+        self.build()
+
+    def build(self):
+        width = 500
+        height = 100
+        title = self.sheet.getTitle()
+        txt = 'Enter name for sheet "' + title + '":'
+
+        self.setWindowTitle('Xmind Import')
+        self.setWindowIcon(QIcon(os.path.join(ICONS_PATH, "icon.png")))
+        self.resize(width, height)
+
+        layout = QtWidgets.QWidget(self)
+        layout.setGeometry(QtCore.QRect(10, 10, width - 20, height - 20))
+
+        v_layout = QtWidgets.QVBoxLayout(layout)
+        v_layout.setContentsMargins(0, 0, 0, 0)
+
+        h_layout_1 = QtWidgets.QHBoxLayout()
+        label = QtWidgets.QLabel(layout)
+        label.setText(txt)
+        h_layout_1.addWidget(label)
+
+        h_layout_2 = QtWidgets.QHBoxLayout()
+        user_input = QtWidgets.QLineEdit(layout)
+        user_input.setText(title)
+        h_layout_2.addWidget(user_input)
+
+        h_layout_3 = QtWidgets.QHBoxLayout()
+        buttons = QtWidgets.QDialogButtonBox(layout)
+        buttons.setStandardButtons(
+            QtWidgets.QDialogButtonBox.Cancel | QtWidgets.QDialogButtonBox.Ok)
+        h_layout_3.addWidget(buttons)
+
+        v_layout.addLayout(h_layout_1)
+        v_layout.addLayout(h_layout_2)
+        v_layout.addLayout(h_layout_3)
+
+        frame = self.frameGeometry()
+        center = QDesktopWidget().availableGeometry().center()
+        frame.moveCenter(center)
+        self.move(frame.topLeft())
