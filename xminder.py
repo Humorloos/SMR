eea8c5e0b66d8bb126c9b186e5e17509378a5640
Humorloos <l.loos95@gmail.com>
2019-09-08 10:50:44 +0200

    add method get_x_sheets() for getting sheets and sheet-names via gui

--- xminder.py
+++ xminder.py
@@ -1,8 +1,32 @@
 from anki.importing.noteimp import NoteImporter
 
+from xxmind import load
+from sheetselectors import *
+
+from XmindImport.consts import *
 
 class XmindImporter(NoteImporter):
     needMapper = False
 
+    def __init__(self, col, file):
+        NoteImporter.__init__(self, col, file)
+        self.model = col.models.byName(X_MODEL_NAME)
+        self.sheets = None
+
+
     def run(self):
-        self.log = ['Xmind Datei wurde importiert.']
+        self.log = ['fertig']
+        self.get_x_sheets()
+
+
+    def get_x_sheets(self):
+        mw = aqt.mw.app.activeWindow() or aqt.mw
+        mw.progress.finish()
+        doc = load(self.file)
+        imp_sheets = doc.getSheets()
+        if len(imp_sheets) > 1:
+            selector = MultiSheetSelector(imp_sheets)
+        else:
+            selector = SingleSheetSelector(imp_sheets[0])
+        selector.exec_()
+        return
