4ff6269b61e72d5d0f16a056e891a8e77d2e9e95
Humorloos <l.loos95@gmail.com>
2019-09-15 10:44:49 +0200

    add test for function import_map()

--- tests/test_xminder.py
+++ tests/test_xminder.py
@@ -1,7 +1,9 @@
 import os
 
+from xxmind import load
+
 from tests.shared import getEmptyCol
-from xminder import XmindImporter
+from xminder import XmindImporter, SheetImport
 from XmindImport.consts import ADDON_PATH
 
 def test_get_x_sheet():
@@ -9,3 +11,12 @@ def test_get_x_sheet():
     file = os.path.join(ADDON_PATH, 'tests', 'support', 'testmap1sheet.xmind')
     importer = XmindImporter(col=col, file=file)
     sheets = importer.get_x_sheets()
+
+def test_import_map():
+    col = getEmptyCol()
+    file = os.path.join(ADDON_PATH, 'tests', 'support', 'testmapmultsheet.xmind')
+    importer = XmindImporter(col=col, file=file)
+    doc = load(file)
+    sheet = doc.getSheets()[0]
+    sheet_import = SheetImport(sheet, 'hi')
+    importer.importMap()
