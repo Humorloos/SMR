62572bef6576cfd52073fc70435d3d249bec6ad0
Humorloos <l.loos95@gmail.com>
2019-10-23 17:32:06 +0200

    add function for constructing anki addon file for sharing

---
+++ zipYourself.py
@@ -0,0 +1,50 @@
+# import required modules
+
+import os
+import zipfile
+
+
+# Declare the function to return all file paths of the particular directory
+def retrieve_file_paths(dirName):
+    # setup file paths variable
+    filePaths = []
+    exclude = {'.git', 'tests', '__pycache__'}
+    # Read all directory, subdirectories and file lists
+    for root, directories, files in os.walk(dirName):
+        directories[:] = [d for d in directories if d not in exclude]
+        for filename in files:
+            if not (filename == '.gitignore'):
+                # Create the full filepath by using os module.
+                filePath = os.path.join(root, filename)
+                filePaths.append(filePath)
+
+    # return all paths
+    return filePaths
+
+
+# Declare the main function
+def main():
+    # Assign the name of the directory to zip
+    dir_name = os.getcwd()
+
+    # Call the function to retrieve all files and folders of the assigned directory
+    filePaths = retrieve_file_paths(dir_name)
+
+    # printing the list of all files to be zipped
+    print('The following list of files will be zipped:')
+    for fileName in filePaths:
+        print(fileName)
+
+    # writing files to a zipfile
+    zip_file = zipfile.ZipFile(file=dir_name + '.ankiaddon', mode='w')
+    with zip_file:
+        # writing each file one by one
+        for file in filePaths:
+            zip_file.write(filename=file, arcname=file[len(dir_name):])
+
+    print(dir_name + '.zip file is created successfully!')
+
+
+# Call the main function
+if __name__ == "__main__":
+    main()
