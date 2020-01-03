2d28ee4971ce31d979650b4db0dab1d0c9a2b3f5
Humorloos <l.loos95@gmail.com>
2020-01-03 17:23:37 +0100

    add Test for function getNodeImg()

--- tests/test_getNodeContent.py
+++ tests/test_getNodeContent.py
@@ -6,7 +6,7 @@ from unittest import TestCase
 from bs4 import BeautifulSoup
 
 from XmindImport.consts import ADDON_PATH
-from XmindImport.utils import getNodeContent, getNodeTitle
+from XmindImport.utils import getNodeContent, getNodeTitle, getNodeImg
 
 SUPPORT_PATH = os.path.join(ADDON_PATH, 'tests', 'support')
 
@@ -33,3 +33,12 @@ class TestGetNodeTitle(TestCase):
             tag = BeautifulSoup(file.read(), features='html.parser').topic
         act = getNodeTitle(tag)
         self.assertEqual(act, 'biological psychology')
+
+
+class TestGetNodeImg(TestCase):
+    def test_getNodeImage(self):
+        with open(os.path.join(SUPPORT_PATH, 'xmindImporter',
+                               'sheet_biological_psychology.xml'), 'r') as file:
+            tag = BeautifulSoup(file.read(), features='html.parser').topic
+        act = getNodeImg(tag)
+        self.assertEqual(act, None)
