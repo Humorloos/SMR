3bce47b45653e65917dab170729fa91f9b7ff614
Humorloos <l.loos95@gmail.com>
2020-01-09 12:22:28 +0100

    add attempt at saving an ontology for test purposes that does not yet work

--- tests/support/pickler.py
+++ tests/support/pickler.py
@@ -3,11 +3,13 @@
 import os
 import pickle
 import sys
+import owlready2
 
 from bs4 import BeautifulSoup
 from PyQt5 import QtWidgets
 
 from tests.shared import getEmptyCol
+from anki import Collection
 
 from XmindImport.consts import ADDON_PATH
 from XmindImport.xmindimport import XmindImporter
@@ -57,7 +59,33 @@ def getSheetBiologicalPsychology():
                            'sheet_biological_psychology.xml'), 'r') as file:
         return BeautifulSoup(file.read(), features='html.parser')
 
+# def getOntologyBiologicalPsychology():
+#     colPath = os.path.join(SUPPORT_PATH, 'collection.anki2')
+#     col = Collection(colPath)
+#     map = os.path.join(ADDON_PATH, 'resources', 'example map.xmind')
+#     xmindImporter = XmindImporter(col=col, file=map)
+#     xmindImporter.deckId = '1'
+#     xmindImporter.currentSheetImport = 'biological psychology'
+#     xmindImporter.activeManager = xmindImporter.xManagers[0]
+#     xid = '0pbme7b9sg9en8qqmmn9jj06od'
+#     nodeTag = xmindImporter.activeManager.getTagById(xid)
+#     concept = xmindImporter.onto.Root('biological psychology')
+#     concept.Image = None
+#     concept.Media = None
+#     concept.Xid = xid
+#     concept = [concept]
+#     answerDict = {'nodeTag': nodeTag, 'isAnswer': True, 'aId': str(0),
+#                   'crosslink': None, 'concepts': concept}
+#     act = xmindImporter.getQuestions(parentAnswerDict=answerDict,
+#                                 ref='biological psychology')
+#     output = os.path.join(SUPPORT_PATH, 'ontology_biological_psychology.rdf')
+#     xmindImporter.onto.save(file=output, format="rdfxml")
+#     # TODO: figure out how to store the ontology properly, currently when
+#      loading the ontology, properties and individuals cannot be accessed
+#     return owlready2.get_ontology(output).load()
+
 
 sheetImports = getSheetImports()
 selectedSheets = getSelectedSheets()
 sheetBiologicalPsychology = getSheetBiologicalPsychology()
+# ontologyBiologicalPsychology = getOntologyBiologicalPsychology()
