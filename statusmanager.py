f23df67d52f677223fdaa7af74afdceb810cc3f9
Humorloos <l.loos95@gmail.com>
2020-01-23 18:15:42 +0100

    Add functions add_new() and save() to statusmanager.py
    Add_new() is for adding new status files to the status after import and
    save is for saving the status as a json file in user_files

--- statusmanager.py
+++ statusmanager.py
@@ -1,3 +1,24 @@
+import json
+import os
+
+from .consts import ADDON_PATH
+
+
 class StatusManager:
-    def __init__(self):
-        print()
+    def __init__(self, status_file=None):
+        if status_file:
+            self.status_file = status_file
+        else:
+            self.status_file = os.path.join(
+                ADDON_PATH, 'user_files', 'status.json')
+        try:
+            self.status = json.loads(self.status_file)
+        except json.decoder.JSONDecodeError:
+            self.status = []
+
+    def add_new(self, status):
+        self.status.append(status)
+
+    def save(self):
+        with open(self.status_file, 'w') as status_file:
+            json.dump(self.status, status_file)
