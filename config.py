14dca4f3f8c3f3a25251ae894bdcc13e18bf99d7
Humorloos <l.loos95@gmail.com>
2020-02-14 18:47:25 +0100

    Add function look_up_version and skeleton for set_up_ontology()

--- config.py
+++ config.py
@@ -14,3 +14,16 @@ def get_or_create_model():
             LooseVersion(model['vers'][-1]) < LooseVersion(X_MODEL_VERSION):
         update_x_model(mw.col)
     return model
+
+
+def look_up_version():
+    if 'smr' not in mw.col.conf:
+        set_up_ontology()
+    if 'smr' not in mw.col.conf or \
+            LooseVersion(mw.col.conf['smr']['version']) < \
+            LooseVersion(SMR_CONFIG['version']):
+        mw.col.conf['smr'] = SMR_CONFIG
+
+
+def set_up_ontology():
+    pass
