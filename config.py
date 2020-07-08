b27e0ad3d81df91e3d7696655b7b165f68b788d5
Lukas Loos <l.loos95@gmail.com>
2020-07-08 18:05:16 +0200

    Add tests for get_or_create_model()

--- config.py
+++ config.py
@@ -1,18 +1,23 @@
-from .ximports.xversion import LooseVersion
+import os
+
+from consts import X_MODEL_NAME, X_MODEL_VERSION, SMR_CONFIG, USER_PATH
+from owlready2.namespace import World
+from template import update_x_model, add_x_model
+from ximports.xversion import LooseVersion
 
 from aqt import mw
 
-from .template import *
-from owlready2.namespace import World
-
 
 def get_or_create_model():
+    """
+    Creates the SMR model if it is not yet created and returns it.
+    :return: the SMR model
+    """
     model = mw.col.models.byName(X_MODEL_NAME)
     if not model:
         # create model
         model = add_x_model(mw.col)
-    if len(model['vers']) == 0 or \
-            LooseVersion(model['vers'][-1]) < LooseVersion(X_MODEL_VERSION):
+    if len(model['vers']) == 0 or LooseVersion(model['vers'][-1]) < LooseVersion(X_MODEL_VERSION):
         update_x_model(mw.col)
     return model
 
--- tests/test_config.py
+++ tests/test_config.py
@@ -1,8 +1,42 @@
-from unittest import TestCase
-from XmindImport.config import set_up_ontology
+import config
+from consts import X_MODEL_NAME, X_MODEL_VERSION
 
 
-class Test(TestCase):
-    def test_set_up_ontology(self):
-        set_up_ontology()
-        self.fail()
+def test_get_or_create_model_model_up_to_date(mocker):
+    # given
+    models = {X_MODEL_NAME: {'vers': [X_MODEL_VERSION]}}
+    mocker.patch("config.mw")
+    config.mw.col.models.byName.side_effect = models.get
+    # when
+    act = config.get_or_create_model()
+    # then
+    assert act == models[X_MODEL_NAME]
+    assert config.mw.col.models.byName.call_count == 1
+
+
+def test_get_or_create_model_model_out_of_date(mocker):
+    # given
+    models = {X_MODEL_NAME: {'vers': ['0']}}
+    mocker.patch("config.mw")
+    config.mw.col.models.byName.side_effect = models.get
+    mocker.patch("config.update_x_model")
+    # when
+    act = config.get_or_create_model()
+    # then
+    assert act == models[X_MODEL_NAME]
+    assert config.mw.col.models.byName.call_count == 1
+    assert config.update_x_model.call_count == 1
+
+
+def test_get_or_create_model_no_model(mocker):
+    # given
+    models = {}
+    mocker.patch("config.mw")
+    config.mw.col.models.byName.side_effect = models.get
+    mocker.patch("config.update_x_model")
+    # when
+    act = config.get_or_create_model()
+    # then
+    assert act == models[X_MODEL_NAME]
+    assert config.mw.col.models.byName.call_count == 1
+    assert config.update_x_model.call_count == 1
